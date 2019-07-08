f1e8c6ec5137f3aad8eec2643e2bf300e789970b
Magdiel <mgdlgj@gmail.com>
2019-07-08 09:59:36 -0500

    Update process_dataset.py

--- process_dataset.py
+++ process_dataset.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
 #-------------------------------------------------------------------------------
 # Author: Lukasz Janyst <lukasz@jany.st>
-# Date:   29.08.2017
+# Date:   30.08.2017
 #-------------------------------------------------------------------------------
 # This file is part of SSD-TensorFlow.
 #
@@ -19,239 +18,173 @@
 # along with SSD-Tensorflow.  If not, see <http://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------------------
 
-import argparse
-import pickle
-import sys
+import lxml.etree
+import random
+import math
 import cv2
 import os
 
 import numpy as np
 
-from transforms import *
-from ssdutils import get_preset_by_name
-from utils import load_data_source, str2bool, draw_box
+from utils import Label, Box, Sample, Size
+from utils import rgb2bgr, abs2prop
+from glob import glob
 from tqdm import tqdm
 
-if sys.version_info[0] < 3:
-    print("This is a Python 3 program. Use Python 3 or higher.")
-    sys.exit(1)
+#-------------------------------------------------------------------------------
+# Labels
+#-------------------------------------------------------------------------------
+'''
+label_defs = [
+    Label('aeroplane',   rgb2bgr((0,     0,   0))),
+    Label('bicycle',     rgb2bgr((111,  74,   0))),
+    Label('bird',        rgb2bgr(( 81,   0,  81))),
+    Label('boat',        rgb2bgr((128,  64, 128))),
+    Label('bottle',      rgb2bgr((244,  35, 232))),
+    Label('bus',         rgb2bgr((230, 150, 140))),
+    Label('car',         rgb2bgr(( 70,  70,  70))),
+    Label('cat',         rgb2bgr((102, 102, 156))),
+    Label('chair',       rgb2bgr((190, 153, 153))),
+    Label('cow',         rgb2bgr((150, 120,  90))),
+    Label('diningtable', rgb2bgr((153, 153, 153))),
+    Label('dog',         rgb2bgr((250, 170,  30))),
+    Label('horse',       rgb2bgr((220, 220,   0))),
+    Label('motorbike',   rgb2bgr((107, 142,  35))),
+    Label('person',      rgb2bgr(( 52, 151,  52))),
+    Label('pottedplant', rgb2bgr(( 70, 130, 180))),
+    Label('sheep',       rgb2bgr((220,  20,  60))),
+    Label('sofa',        rgb2bgr((  0,   0, 142))),
+    Label('train',       rgb2bgr((  0,   0, 230))),
+    Label('tvmonitor',   rgb2bgr((119,  11,  32)))
+   ]
+'''
+
+label_defs = [Label('person',rgb2bgr(( 52, 151,  52)))]
 
 #-------------------------------------------------------------------------------
-def annotate(data_dir, samples, colors, sample_name):
-    """
-    Draw the bounding boxes on the sample images
-    :param data_dir: the directory where the dataset's files are stored
-    :param samples:  samples to be processed
-    :param colors:   a dictionary mapping class name to a BGR color tuple
-    :param colors:   name of the sample
-    """
-    result_dir = data_dir+'/annotated/'+sample_name.strip()+'/'
-    if not os.path.exists(result_dir):
-        os.makedirs(result_dir)
-
-    for sample in tqdm(samples, desc=sample_name, unit='samples'):
-        img    = cv2.imread(sample.filename)
-        basefn = os.path.basename(sample.filename)
-        for box in sample.boxes:
-            draw_box(img, box, colors[box.label])
-        cv2.imwrite(result_dir+basefn, img)
-
-#-------------------------------------------------------------------------------
-def build_sampler(overlap, trials):
-    return SamplerTransform(sample=True, min_scale=0.3, max_scale=1.0,
-                            min_aspect_ratio=0.5, max_aspect_ratio=2.0,
-                            min_jaccard_overlap=overlap, max_trials=trials)
-
-#-------------------------------------------------------------------------------
-def build_train_transforms(preset, num_classes, sampler_trials, expand_prob):
+class PascalVOCSource:
     #---------------------------------------------------------------------------
-    # Resizing
+    def __init__(self):
+        self.num_classes   = len(label_defs)
+        self.colors        = {l.name: l.color for l in label_defs}
+        self.lid2name      = {i: l.name for i, l in enumerate(label_defs)}
+        self.lname2id      = {l.name: i for i, l in enumerate(label_defs)}
+        self.num_train     = 0
+        self.num_valid     = 0
+        self.num_test      = 0
+        self.train_samples = []
+        self.valid_samples = []
+        self.test_samples  = []
+
+
+
+    def __build_annotation_list( self,root, dataset_type):        
+        annot_root  = root + '/Annotations/'
+        annot_files = []
+        with open(root + '/ImageSets/Main/' + dataset_type + '.txt') as f:
+            for line in f:
+                pos=len(line.split(' '))-1
+                imgid=int(line.split(' ')[pos])
+                if imgid==1:
+                    dat=line.split(' ')[0]
+                    annot_file = annot_root + dat + '.xml'
+                    if os.path.exists(annot_file):
+                        annot_files.append(annot_file)
+        return annot_files
     #---------------------------------------------------------------------------
-    tf_resize = ResizeTransform(width=preset.image_size.w,
-                                height=preset.image_size.h,
-                                algorithms=[cv2.INTER_LINEAR,
-                                            cv2.INTER_AREA,
-                                            cv2.INTER_NEAREST,
-                                            cv2.INTER_CUBIC,
-                                            cv2.INTER_LANCZOS4])
+    def __build_sample_list(self, root, annot_files, dataset_name):
+        """
+        Build a list of samples for the VOC dataset (either trainval or test)
+        """
+        image_root  = root + '/JPEGImages/'
+        samples     = []
 
-    #---------------------------------------------------------------------------
-    # Image distortions
-    #---------------------------------------------------------------------------
-    tf_brightness = BrightnessTransform(delta=32)
-    tf_rnd_brightness = RandomTransform(prob=0.5, transform=tf_brightness)
+        #-----------------------------------------------------------------------
+        # Process each annotated sample
+        #-----------------------------------------------------------------------
+        for fn in tqdm(annot_files, desc=dataset_name, unit='samples'):
+            with open(fn, 'r') as f:
+                doc      = lxml.etree.parse(f)
+                filename = image_root+doc.xpath('/annotation/filename')[0].text
+                if not os.path.exists(filename):
+                    continue
+                img     = cv2.imread(filename)
+                imgsize = Size(img.shape[1], img.shape[0])
+                boxes    = []
+                objects  = doc.xpath('/annotation/object')
+                for obj in objects:
+                    label = obj.xpath('name')[0].text
+                    if label==label_defs[0].name:
+                        xmin  = int(float(obj.xpath('bndbox/xmin')[0].text))
+                        xmax  = int(float(obj.xpath('bndbox/xmax')[0].text))
+                        ymin  = int(float(obj.xpath('bndbox/ymin')[0].text))
+                        ymax  = int(float(obj.xpath('bndbox/ymax')[0].text))
+                        center, size = abs2prop(xmin, xmax, ymin, ymax, imgsize)
+                        box = Box(label, self.lname2id[label], center, size)
+                        boxes.append(box)
+                if not boxes:
+                    continue
+                sample = Sample(filename, boxes, imgsize)
+                samples.append(sample)
 
-    tf_contrast = ContrastTransform(lower=0.5, upper=1.5)
-    tf_rnd_contrast = RandomTransform(prob=0.5, transform=tf_contrast)
-
-    tf_hue = HueTransform(delta=18)
-    tf_rnd_hue = RandomTransform(prob=0.5, transform=tf_hue)
-
-    tf_saturation = SaturationTransform(lower=0.5, upper=1.5)
-    tf_rnd_saturation = RandomTransform(prob=0.5, transform=tf_saturation)
-
-    tf_reorder_channels = ReorderChannelsTransform()
-    tf_rnd_reorder_channels = RandomTransform(prob=0.5,
-                                              transform=tf_reorder_channels)
-
-    #---------------------------------------------------------------------------
-    # Compositions of image distortions
-    #---------------------------------------------------------------------------
-    tf_distort_lst = [
-        tf_rnd_contrast,
-        tf_rnd_saturation,
-        tf_rnd_hue,
-        tf_rnd_contrast
-    ]
-    tf_distort_1 = ComposeTransform(transforms=tf_distort_lst[:-1])
-    tf_distort_2 = ComposeTransform(transforms=tf_distort_lst[1:])
-    tf_distort_comp = [tf_distort_1, tf_distort_2]
-    tf_distort = TransformPickerTransform(transforms=tf_distort_comp)
+        return samples
 
     #---------------------------------------------------------------------------
-    # Expand sample
-    #---------------------------------------------------------------------------
-    tf_expand = ExpandTransform(max_ratio=4.0, mean_value=[104, 117, 123])
-    tf_rnd_expand = RandomTransform(prob=expand_prob, transform=tf_expand)
+    def load_trainval_data(self, data_dir, valid_fraction):
+
+        nameds='person_train' #nombre del datas
+        train_annot = []
+        train_samples = []
+        for vocid in ['VOC2007', 'VOC2012']:
+            root = data_dir + '/trainval/VOCdevkit/'+vocid
+            name = 'trainval_'+vocid
+            annot = self.__build_annotation_list(root, nameds)
+            train_annot += annot
+            train_samples += self.__build_sample_list(root, annot, name)
+
+        root = data_dir + '/test/VOCdevkit/VOC2007'
+        annot = self.__build_annotation_list(root, 'person_test')
+        train_samples += self.__build_sample_list(root, annot, 'test_VOC2007')
+
+
+        root = data_dir + '/trainval/VOCdevkit/VOC2012'
+        all_annot = set(glob(root + '/Annotations/*.xml'))
+        valid_annot = all_annot - set(train_annot)
+        valid_samples = self.__build_sample_list(root, valid_annot,
+                                                 'valid_VOC2012')
+
+        #-----------------------------------------------------------------------
+        # Final set up and sanity check
+        #-----------------------------------------------------------------------
+        self.valid_samples = valid_samples
+        self.train_samples = train_samples
+
+        if len(self.train_samples) == 0:
+            raise RuntimeError('No training samples found in ' + data_dir)
+
+        if valid_fraction > 0:
+            if len(self.valid_samples) == 0:
+                raise RuntimeError('No validation samples found in ' + data_dir)
+
+        self.num_train = len(self.train_samples)
+        self.num_valid = len(self.valid_samples)
 
     #---------------------------------------------------------------------------
-    # Samplers
-    #---------------------------------------------------------------------------
-    samplers = [
-        SamplerTransform(sample=False),
-        build_sampler(0.1, sampler_trials),
-        build_sampler(0.3, sampler_trials),
-        build_sampler(0.5, sampler_trials),
-        build_sampler(0.7, sampler_trials),
-        build_sampler(0.9, sampler_trials),
-        build_sampler(1.0, sampler_trials)
-    ]
-    tf_sample_picker = SamplePickerTransform(samplers=samplers)
+    def load_test_data(self, data_dir):
+        """
+        Load the test data
+        :param data_dir: the directory where the dataset's file are stored
+        """
+        root = data_dir + '/test/VOCdevkit/VOC2012'
+        annot = self.__build_annotation_list(root, 'test')
+        self.test_samples  = self.__build_sample_list(root, annot,
+                                                      'test_VOC2012')
 
-    #---------------------------------------------------------------------------
-    # Horizontal flip
-    #---------------------------------------------------------------------------
-    tf_flip = HorizontalFlipTransform()
-    tf_rnd_flip = RandomTransform(prob=0.5, transform=tf_flip)
+        if len(self.test_samples) == 0:
+            raise RuntimeError('No testing samples found in ' + data_dir)
 
-    #---------------------------------------------------------------------------
-    # Transform list
-    #---------------------------------------------------------------------------
-    transforms = [
-        ImageLoaderTransform(),
-        tf_rnd_brightness,
-        tf_distort,
-        tf_rnd_reorder_channels,
-        tf_rnd_expand,
-        tf_sample_picker,
-        tf_rnd_flip,
-        LabelCreatorTransform(preset=preset, num_classes=num_classes),
-        tf_resize
-    ]
-    return transforms
-
-#-------------------------------------------------------------------------------
-def build_valid_transforms(preset, num_classes):
-    tf_resize = ResizeTransform(width=preset.image_size.w,
-                                height=preset.image_size.h,
-                                algorithms=[cv2.INTER_LINEAR])
-    transforms = [
-        ImageLoaderTransform(),
-        LabelCreatorTransform(preset=preset, num_classes=num_classes),
-        tf_resize
-    ]
-    return transforms
+        self.num_test  = len(self.test_samples)
 
 #-------------------------------------------------------------------------------
-def main():
-    #---------------------------------------------------------------------------
-    # Parse the commandline
-    #---------------------------------------------------------------------------
-    parser = argparse.ArgumentParser(description='Process a dataset for SSD')
-    parser.add_argument('--data-source', default='pascal_voc',
-                        help='data source')
-    parser.add_argument('--data-dir', default='pascal-voc',
-                        help='data directory')
-    parser.add_argument('--validation-fraction', type=float, default=0.025,
-                        help='fraction of the data to be used for validation')
-    parser.add_argument('--expand-probability', type=float, default=0.5,
-                        help='probability of running sample expander')
-    parser.add_argument('--sampler-trials', type=int, default=50,
-                        help='number of time a sampler tries to find a sample')
-    parser.add_argument('--annotate', type=str2bool, default='False',
-                        help="Annotate the data samples")
-    parser.add_argument('--compute-td', type=str2bool, default='True',
-                        help="Compute training data")
-    parser.add_argument('--preset', default='vgg300',
-                        choices=['vgg300', 'vgg512'],
-                        help="The neural network preset")
-    parser.add_argument('--process-test', type=str2bool, default='False',
-                        help="process the test dataset")
-    args = parser.parse_args()
-
-    print('[i] Data source:          ', args.data_source)
-    print('[i] Data directory:       ', args.data_dir)
-    print('[i] Validation fraction:  ', args.validation_fraction)
-    print('[i] Expand probability:   ', args.expand_probability)
-    print('[i] Sampler trials:       ', args.sampler_trials)
-    print('[i] Annotate:             ', args.annotate)
-    print('[i] Compute training data:', args.compute_td)
-    print('[i] Preset:               ', args.preset)
-    print('[i] Process test dataset: ', args.process_test)
-
-    #---------------------------------------------------------------------------
-    # Load the data source
-    #---------------------------------------------------------------------------
-    print('[i] Configuring the data source...')
-    try:
-        source = load_data_source(args.data_source)
-        source.load_trainval_data(args.data_dir, args.validation_fraction)
-        if args.process_test:
-            source.load_test_data(args.data_dir)
-        print('[i] # training samples:   ', source.num_train)
-        print('[i] # validation samples: ', source.num_valid)
-        print('[i] # testing samples:    ', source.num_test)
-        print('[i] # classes:            ', source.num_classes)
-    except (ImportError, AttributeError, RuntimeError) as e:
-        print('[!] Unable to load data source:', str(e))
-        return 1
-
-    #---------------------------------------------------------------------------
-    # Annotate samples
-    #---------------------------------------------------------------------------
-    if args.annotate:
-        print('[i] Annotating samples...')
-        annotate(args.data_dir, source.train_samples, source.colors, 'train')
-        annotate(args.data_dir, source.valid_samples, source.colors, 'valid')
-        if args.process_test:
-            annotate(args.data_dir, source.test_samples,  source.colors, 'test ')
-
-    #---------------------------------------------------------------------------
-    # Compute the training data
-    #---------------------------------------------------------------------------
-    if args.compute_td:
-        preset = get_preset_by_name(args.preset)
-        with open(args.data_dir+'/train-samples.pkl', 'wb') as f:
-            pickle.dump(source.train_samples, f)
-        with open(args.data_dir+'/valid-samples.pkl', 'wb') as f:
-            pickle.dump(source.valid_samples, f)
-
-        with open(args.data_dir+'/training-data.pkl', 'wb') as f:
-            data = {
-                'preset': preset,
-                'num-classes': source.num_classes,
-                'colors': source.colors,
-                'lid2name': source.lid2name,
-                'lname2id': source.lname2id,
-                'train-transforms': build_train_transforms(preset,
-                                       source.num_classes, args.sampler_trials,
-                                       args.expand_probability ),
-                'valid-transforms': build_valid_transforms(preset,
-                                                           source.num_classes)
-            }
-            pickle.dump(data, f)
-
-    return 0
-
-if __name__ == '__main__':
-    sys.exit(main())
+def get_source():
+    return PascalVOCSource()
